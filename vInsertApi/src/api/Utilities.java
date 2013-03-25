d7bf959f992bfcd5a430e8c706a2f392fb553e18
fortrucex <rollins.joseph@gmail.com>
2013-03-25 13:27:40 -0500

    Added halveDistance

--- vInsertApi/src/api/Utilities.java
+++ vInsertApi/src/api/Utilities.java
@@ -1,5 +1,14 @@
 package api;
 
-public class Utilities {
+import org.vinsert.bot.script.api.Tile;
 
+public class Utilities {
+	public static Tile halveDistance(Tile a, Tile b) {
+		int x, y;
+		x = a.getX() + b.getX();
+		x /= 2;
+		y = a.getY() + b.getY();
+		y /= 2;
+		return new Tile(x, y);
+	}
 }
