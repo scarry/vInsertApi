038ace62ed3b0ac42e57ce9ed5d800fe3d5c347a
Cory Duplantis <potofreak38732@yahoo.com>
2013-03-25 21:31:02 -0500

    Utilities with ScriptContext

--- vInsertApi/src/api/Utilities.java
+++ vInsertApi/src/api/Utilities.java
@@ -1,8 +1,22 @@
 package api;
 
+import java.awt.Point;
+
+import org.vinsert.bot.script.Script;
+import org.vinsert.bot.script.ScriptContext;
+import org.vinsert.bot.script.api.Item;
 import org.vinsert.bot.script.api.Tile;
+import org.vinsert.bot.script.api.tools.Inventory;
 
 public class Utilities {
+
+	ScriptContext sc;
+	
+	public void Utilites(ScriptContext context){
+		this.sc = context;
+		return;
+	}	
+	
 	public static Tile halveDistance(Tile a, Tile b) {
 		int x, y;
 		x = a.getX() + b.getX();
@@ -11,4 +25,13 @@ public class Utilities {
 		y /= 2;
 		return new Tile(x, y);
 	}
+	
+	public static boolean clickItem(Item item){
+		int slot = sc.inventory.indexOf(item);
+		Point point = sc.inventory.getClickPoint(slot);
+		sc.mouse.click(point.x, point.y);
+		return false;
+	}
+	
+	
 }
