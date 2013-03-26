92545233a737dfcb5915ca71f2dc7a40324ab9d3
cajuninms <potofreak38732@yahoo.com>
2013-03-26 11:38:21 -0500

    Added ProjectionListener to Node

--- vInsertApi/src/api/Node.java
+++ vInsertApi/src/api/Node.java
@@ -1,6 +1,10 @@
 package api;
 
-public abstract class Node {
+import java.awt.Graphics2D;
+
+import org.vinsert.component.ProjectionListener;
+
+public abstract class Node implements ProjectionListener{
 	public abstract boolean activate();
 	public abstract void execute();
 	
@@ -12,4 +16,7 @@ public abstract class Node {
 	public String toString() {
 		return this.getClass().getSimpleName();
 	}
+	
+	@Override
+	public abstract void render(Graphics2D arg0);
 }
