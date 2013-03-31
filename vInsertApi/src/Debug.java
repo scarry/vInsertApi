ea42d7306531880bc390aa360a9048a3f48d19eb
fortrucex <rollins.joseph@gmail.com>
2013-03-30 20:34:13 -0500

    Create Debug.java

---
+++ vInsertApi/src/Debug.java
@@ -0,0 +1,42 @@
+import api.FWidgets;
+import api.Node;
+import api.ScriptBase;
+import org.vinsert.bot.script.ScriptManifest;
+import org.vinsert.bot.script.api.Widget;
+
+import java.awt.*;
+
+/**
+ * Author: Fortruce
+ */
+@ScriptManifest(name = "Debug", authors = {"Fortruce"})
+public class Debug extends ScriptBase {
+
+    private FWidgets fWidgets;
+
+    public class Test extends Node {
+        @Override
+        public boolean activate() {
+            return true;
+        }
+
+        @Override
+        public void execute() {
+            Widget wid = fWidgets.get(137, 0);
+            fWidgets.getGroupIndex(wid);
+        }
+    }
+
+    @Override
+    public boolean init() {
+
+        fWidgets = new FWidgets(getContext());
+
+        submit(new Test());
+        return true;
+    }
+
+    @Override
+    public void render(Graphics2D arg0) {
+    }
+}
